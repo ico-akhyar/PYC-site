0b6460921d6b378655bd1ab0287fb8da8a75f085
akhyar <akhyarahmad919@gmail.com>
2025-09-01 12:17:14 +0500

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDHpg6Olt1oYUP0ivOxKtA2_rHCvPqZPB0",
   authDomain: "pyc-official.firebaseapp.com",
   projectId: "pyc-official",
-  storageBucket: "pyc-official.firebasestorage.app",
+storageBucket: "pyc-official.appspot.com",
   messagingSenderId: "51535591568",
   appId: "1:51535591568:web:c85e0cc5cda07f2ed2c015",
   measurementId: "G-Z5WN7G13W6"
