301653b397b17fc4a508f0dc03aa5453e2f481db
akhyar <akhyarahmad919@gmail.com>
2025-09-01 12:45:45 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,11 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+export default defineConfig({
+  optimizeDeps: {
+    include: ["firebase/app", "firebase/firestore"]
+  }
+});
+
+
 });
